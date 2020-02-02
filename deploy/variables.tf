1065dedac313f2973bb46bd18af949e3862eda19
will-goodman <58141090+will-goodman@users.noreply.github.com>
2020-02-02 17:52:39 +0000

    Added terraform to deploy the UI lambda.

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -21,11 +21,62 @@ variable "subnet_availability_zone" {
 }
 
 variable "public_cidr_range" {
-  description = "CIDR range for the public subnets. Must be within the VPC CIDR range."
+  description = "CIDR range for the first public subnet. Must be within the VPC CIDR range."
   default = "10.0.1.0/24"
 }
 
+variable "second_public_cidr_range" {
+  description = "CIDR range for the second public subnet. Must be within the VPC CIDR range."
+  default = "10.0.3.0/24"
+}
+
 variable "private_cidr_range" {
-  description = "CIDR range for the public subnets. Must be within the VPC CIDR range."
+  description = "CIDR range for the first private subnet. Must be within the VPC CIDR range."
   default = "10.0.2.0/24"
+}
+
+variable "second_private_cidr_range" {
+  description = "CIDR range for the second private subnet. Must be within the VPC CIDR range."
+  default = "10.0.4.0/24"
+}
+
+//UI
+variable "ui_lambda_function_name" {
+  description = "Name of the UI lambda function with the ending dropped e.g. test.py -> test"
+  default = "test"
+}
+
+variable "ui_lambda_handler" {
+  description = "Path to the lambda handler for the UI e.g. /path/file.lambda_handler"
+  default = "test.lambda_handler"
+}
+
+variable "ui_lambda_logs_retention" {
+  description = "How long to store the UI lambda logs in days."
+  default = 7
+}
+
+variable "ui_lambda_memory_size" {
+  description = "Memory to allocate to the UI lambda in MB. Default 128MB."
+  default = 128
+}
+
+variable "ui_lambda_timeout" {
+  description = "Timeout in seconds for the UI lambda. Default 3 seconds."
+  default = 3
+}
+
+variable "ui_lambda_runtime" {
+  description = "Runtime environment for the UI lambda."
+  default = "python3.7"
+}
+
+variable "ui_lambda_filename" {
+  description = "Path to the zip file containing the lambda code."
+  default = "../test.zip"
+}
+
+variable "ui_lambda_sg_name" {
+  description = "Name for the security group assigned to the UI lambda."
+  default = "awswebsite-ui-sg"
 }
