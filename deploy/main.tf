1ccc45d1aa3f7d61a1769721006e84f499e57118
will-goodman <58141090+will-goodman@users.noreply.github.com>
2020-02-08 17:51:36 +0000

    Added terraform to deploy the ALB.

--- deploy/main.tf
+++ deploy/main.tf
@@ -5,7 +5,6 @@ module "vpc" {
   vpc_name = var.vpc_name
 
   vpc_cidr = var.vpc_cidr
-  subnet_availability_zone = var.subnet_availability_zone
   public_cidr_range = var.public_cidr_range
   second_public_cidr_range = var.second_public_cidr_range
   private_cidr_range = var.private_cidr_range
@@ -117,3 +116,134 @@ resource "aws_security_group" "api" {
     cidr_blocks     = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_lb" "alb" {
+  name = var.alb_name
+
+  load_balancer_type = "application"
+
+  security_groups = [aws_security_group.alb.id]
+  subnets = module.vpc.public_subnets
+}
+
+resource "aws_security_group" "alb" {
+  name        = var.alb_sg_name
+  description = "Allow HTTP/S connection."
+  vpc_id      = module.vpc.vpc_id
+
+  #HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  # HTTPS
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "ui_target_group" {
+  name        = var.alb_ui_target_group_name
+  target_type = "lambda"
+}
+
+resource "aws_lambda_permission" "ui_with_lb" {
+  statement_id  = "AllowExecutionFromlb"
+  action        = "lambda:InvokeFunction"
+  function_name = module.ui.arn
+  principal     = "elasticloadbalancing.amazonaws.com"
+  source_arn    = aws_lb_target_group.ui_target_group.arn
+}
+
+resource "aws_lb_target_group_attachment" "ui" {
+  target_group_arn = aws_lb_target_group.ui_target_group.arn
+  target_id        = module.ui.arn
+  depends_on       = [aws_lambda_permission.ui_with_lb]
+}
+
+resource "aws_lb_target_group" "api_target_group" {
+  name        = var.alb_api_target_group_name
+  target_type = "lambda"
+}
+
+resource "aws_lambda_permission" "api_with_lb" {
+  statement_id  = "AllowExecutionFromlb"
+  action        = "lambda:InvokeFunction"
+  function_name = module.api.arn
+  principal     = "elasticloadbalancing.amazonaws.com"
+  source_arn    = aws_lb_target_group.api_target_group.arn
+}
+
+resource "aws_lb_target_group_attachment" "api" {
+  target_group_arn = aws_lb_target_group.api_target_group.arn
+  target_id        = module.api.arn
+  depends_on       = [aws_lambda_permission.api_with_lb]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ui_target_group.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "api_http" {
+  listener_arn = aws_lb_listener.http.arn
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.api_target_group.arn
+  }
+  condition {
+    path_pattern {
+      values = ["/api/*"]
+    }
+  }
+}
+
+//resource "aws_lb_listener" "https" {
+//  load_balancer_arn = module.alb.alb_arn
+//  port              = "443"
+//  protocol          = "HTTPS"
+//
+//  default_action {
+//    type             = "forward"
+//    target_group_arn = aws_lb_target_group.ui_target_group.arn
+//  }
+//}
+//
+//resource "aws_lb_listener_rule" "api_https" {
+//  listener_arn = aws_lb_listener.https.arn
+//  action {
+//    type = "forward"
+//    target_group_arn = aws_lb_target_group.api_target_group.arn
+//  }
+//  condition {
+//    path_pattern {
+//      values = ["/api/*"]
+//    }
+//  }
+//}
