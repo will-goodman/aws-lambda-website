3c6e4f646c2a8d1b9ff26a6ee7676a9c1c20130b
will-goodman <58141090+will-goodman@users.noreply.github.com>
2020-02-10 21:08:30 +0000

    Made ALB module

---
+++ deploy/alb/main.tf
@@ -0,0 +1,131 @@
+
+resource "aws_lb" "alb" {
+  name = var.alb_name
+
+  load_balancer_type = "application"
+
+  security_groups = [aws_security_group.alb.id]
+  subnets = var.subnets
+}
+
+resource "aws_security_group" "alb" {
+  name        = var.alb_sg_name
+  description = "Allow HTTP/S connection."
+  vpc_id      = var.vpc_id
+
+  #HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  # HTTPS
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "ui_target_group" {
+  name        = var.alb_ui_target_group_name
+  target_type = "lambda"
+}
+
+resource "aws_lambda_permission" "ui_with_lb" {
+  statement_id  = "AllowExecutionFromlb"
+  action        = "lambda:InvokeFunction"
+  function_name = var.ui_lambda_arn
+  principal     = "elasticloadbalancing.amazonaws.com"
+  source_arn    = aws_lb_target_group.ui_target_group.arn
+}
+
+resource "aws_lb_target_group_attachment" "ui" {
+  target_group_arn = aws_lb_target_group.ui_target_group.arn
+  target_id        = var.ui_lambda_arn
+  depends_on       = [aws_lambda_permission.ui_with_lb]
+}
+
+resource "aws_lb_target_group" "api_target_group" {
+  name        = var.alb_api_target_group_name
+  target_type = "lambda"
+}
+
+resource "aws_lambda_permission" "api_with_lb" {
+  statement_id  = "AllowExecutionFromlb"
+  action        = "lambda:InvokeFunction"
+  function_name = var.api_lambda_arn
+  principal     = "elasticloadbalancing.amazonaws.com"
+  source_arn    = aws_lb_target_group.api_target_group.arn
+}
+
+resource "aws_lb_target_group_attachment" "api" {
+  target_group_arn = aws_lb_target_group.api_target_group.arn
+  target_id        = var.api_lambda_arn
+  depends_on       = [aws_lambda_permission.api_with_lb]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ui_target_group.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "api_http" {
+  listener_arn = aws_lb_listener.http.arn
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.api_target_group.arn
+  }
+  condition {
+    path_pattern {
+      values = ["/api/*"]
+    }
+  }
+}
+
+//resource "aws_lb_listener" "https" {
+//  load_balancer_arn = module.alb.alb_arn
+//  port              = "443"
+//  protocol          = "HTTPS"
+//
+//  default_action {
+//    type             = "forward"
+//    target_group_arn = aws_lb_target_group.ui_target_group.arn
+//  }
+//}
+//
+//resource "aws_lb_listener_rule" "api_https" {
+//  listener_arn = aws_lb_listener.https.arn
+//  action {
+//    type = "forward"
+//    target_group_arn = aws_lb_target_group.api_target_group.arn
+//  }
+//  condition {
+//    path_pattern {
+//      values = ["/api/*"]
+//    }
+//  }
+//}
