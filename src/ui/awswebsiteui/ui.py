587639cb4a4c5565cd3a65fbfea87e84e0ce80f2
will-goodman <58141090+will-goodman@users.noreply.github.com>
2020-02-14 23:41:12 +0000

    Added more debugging lines

--- src/ui/awswebsiteui/ui.py
+++ src/ui/awswebsiteui/ui.py
@@ -12,12 +12,14 @@ def status():
 
 @app.route('/index')
 def index():
+    print("index")
     return app.send_static_file('./aws-lambda-website/index.html')
 
 
 @app.route('/dist/<path:path>')
 def dist(path):
-    return send_from_directory('dist', path)
+    print(path)
+    return send_from_directory('./aws-lambda-website/dist', path)
 
 
 def lambda_handler(event, context):
