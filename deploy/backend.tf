f4c702fe72cc4b0e1c36f4d9e13dff342912c5aa
will-goodman <58141090+will-goodman@users.noreply.github.com>
2020-03-08 20:10:57 +0000

    Removed backend

--- deploy/backend.tf
+++ deploy/backend.tf
@@ -1,11 +1,7 @@
 
 terraform {
   required_version = ">=0.12.19"
-  backend "s3" {
-    bucket = "m9w4y-terraform-state"
-    key = "awslambdawebsite.tfstate"
-    region = "eu-west-2"
-    dynamodb_table = "tf-state-lock-table"
-    encrypt = true
-  }
+
+  # Add backend here if required
+
 }
